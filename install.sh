17954828c070d8e37e6f62ad24c7a67e35dbe6b2
Marek Vavrecan <vavrecan@gmail.com>
2020-09-30 00:40:26 +0200

    install script - error checking

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 mkdir keeweb-local-server
-cd keeweb-local-server
+cd keeweb-local-server || exit
 
 wget https://github.com/keeweb/keeweb/releases/download/v1.15.7/KeeWeb-1.15.7.html.zip
+if [ $? -ne 0 ]; then
+  echo "Failed to download keeweb"
+  exit
+fi
+
 unzip KeeWeb-1.15.7.html.zip
 
 wget https://github.com/vavrecan/keeweb-local-server/archive/master.zip
+if [ $? -ne 0 ]; then
+  echo "Failed to download keeweb-local-server"
+  exit
+fi
+
 unzip master.zip
 
 mv keeweb-local-server-master/plugins .
