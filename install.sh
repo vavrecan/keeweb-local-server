81e7163a70db2ace6260d3036d3d1ed13580f111
Marek Vavrecan <vavrecan@gmail.com>
2021-01-07 16:05:57 +0100

    added password prompt to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 VERSION=1.16.7
+read -s -p "Enter file access password (This is NOT database password and it should be different from database password): " PASSWORD
+
 mkdir keeweb-local-server
 cd keeweb-local-server || exit
 
@@ -27,6 +29,7 @@ mv keeweb-local-server-master/config.json .
 rm -r keeweb-local-server-master
 
 sed -i 's/content="(no-config)"/content="config.json"/' index.html
+sed -i "s/define(\"PASSWORD\", \"[^\"]\+\")/define(\"PASSWORD\", \"${PASSWORD}\")/" server.php
 
 unlink KeeWeb-${VERSION}.html.zip
 unlink master.zip
